03b4b86bd31c917917b53622377ea42a012f2f36
Max Melentiev <melentievm@gmail.com>
2019-01-16 16:31:46 +0530

    Cache regexps for Name::Extract
    This brings 30x speed improvement.

--- lib/fias/config.rb
+++ lib/fias/config.rb
@@ -17,6 +17,10 @@ module Fias
     attr_reader :index, :longs, :shorts, :aliases, :exceptions, :replacements
     attr_reader :proper_names, :synonyms, :synonyms_index
 
+    def cache
+      @cache ||= {}
+    end
+
     def add_name(long, short, aliases = [])
 
       @index
